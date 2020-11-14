895ae02ee6411341ae559abb736d4d9a9d924d58
TimIsOverpowered <admin@angelthump.com>
2020-11-14 14:10:24 +0000

    rate limit

--- src/services/vods/vods.service.js
+++ src/services/vods/vods.service.js
@@ -1,19 +1,29 @@
 // Initializes the `vods` service on path `/vods`
-const { Vods } = require('./vods.class');
-const createModel = require('../../models/vods.model');
-const hooks = require('./vods.hooks');
+const { Vods } = require("./vods.class");
+const createModel = require("../../models/vods.model");
+const hooks = require("./vods.hooks");
+const rateLimit = require("express-rate-limit");
 
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
   };
 
   // Initialize our service with any options it requires
-  app.use('/vods', new Vods(options, app));
+  app.use(
+    "/vods",
+    rateLimit({
+      windowMs: 30 * 1000,
+      max: 10,
+      message:
+        "API rate limit exceeded",
+    }),
+    new Vods(options, app)
+  );
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('vods');
+  const service = app.service("vods");
 
   service.hooks(hooks);
 };
