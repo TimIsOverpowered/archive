fab88b1e472e22ae68d60c9ec063481bf3e26e96
TimIsOverpowered <admin@angelthump.com>
2020-12-03 16:30:11 +0100

    add manual logs

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
   app.post("/twitch/webhook/stream/:userId", webhook.stream(app));
   app.post("/admin/download", admin.verify(app), admin.download(app));
   app.post("/admin/logs", admin.verify(app), admin.logs(app));
+  app.post("/admin/logs/manual", admin.verify(app), admin.manualLogs(app));
   app.post("/admin/dmca", admin.verify(app), admin.dmca(app));
   app.delete("/admin/delete", admin.verify(app), admin.delete(app));
 };
