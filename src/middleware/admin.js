1f375e02d6463a8d84929eccb4106cb283fa3d5d
TimIsOverpowered <admin@angelthump.com>
2021-05-16 00:38:02 +0200

    part dmca

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -540,6 +540,146 @@ module.exports.reUploadPart = function (app) {
   };
 };
 
+module.exports.partDmca = function (app) {
+  return async function (req, res, next) {
+    if (!req.body.receivedClaims)
+      return res.status(400).json({ error: true, message: "No claims" });
+
+    if (!req.body.vodId)
+      return res.status(400).json({ error: true, message: "No vod id" });
+
+    if (!req.body.part)
+      return res.status(400).json({ error: true, message: "No part" });
+
+    res.status(200).json({
+      error: false,
+      message: `Trimming DMCA Content from ${req.body.vodId} Vod Part ${req.body.part}`,
+    });
+
+    let vod_data;
+    await app
+      .service("vods")
+      .get(req.body.vodId)
+      .then((data) => {
+        vod_data = data;
+      })
+      .catch(() => {});
+
+    if (!vod_data) return console.error("Failed get vod: no VOD in database");
+
+    const part = parseInt(req.body.part) - 1;
+    const dir = `${config.vodPath}${req.body.vodId}`;
+    const m3u8Path = `${dir}/${req.body.vodId}.m3u8`;
+    const m3u8Exists = await fileExists(m3u8Path);
+    let trimmedPath;
+    if (m3u8Exists) {
+      trimmedPath = await vod.trimHLS(
+        m3u8Path,
+        req.body.vodId,
+        config.splitDuration * part,
+        config.splitDuration
+      );
+      fs.unlinkSync(m3u8Path);
+    } else {
+      const mp4Video = await vod.download(req.body.vodId);
+
+      if (mp4Video) {
+        trimmedPath = await vod.trim(
+          mp4Video,
+          req.body.vodId,
+          config.splitDuration * part,
+          config.splitDuration
+        );
+        fs.unlinkSync(mp4Video);
+      } else {
+        return console.error(`Could not find a download source for ${req.body.vodId}`);
+      }
+    }
+
+    let muteSection = [],
+      newVodPath,
+      blackoutPath;
+    for (let dmca of req.body.receivedClaims) {
+      //check if audio
+      if (dmca.type === "CLAIM_TYPE_AUDIO") {
+        muteSection.push(
+          `volume=0:enable='between(t,${
+            dmca.matchDetails.longestMatchStartTimeSeconds
+          },${
+            parseInt(dmca.matchDetails.longestMatchDurationSeconds) +
+            parseInt(dmca.matchDetails.longestMatchStartTimeSeconds)
+          })'`
+        );
+      } else if (dmca.type === "CLAIM_TYPE_VISUAL") {
+        console.info(
+          `Trying to blackout ${
+            blackoutPath ? blackoutPath : trimmedPath
+          }. Claim: ${JSON.stringify(dmca.asset.metadata)}`
+        );
+        blackoutPath = await vod.blackoutVideo(
+          blackoutPath ? blackoutPath : trimmedPath,
+          req.body.vodId,
+          dmca.matchDetails.longestMatchStartTimeSeconds,
+          dmca.matchDetails.longestMatchDurationSeconds,
+          parseInt(dmca.matchDetails.longestMatchStartTimeSeconds) +
+            parseInt(dmca.matchDetails.longestMatchDurationSeconds)
+        );
+      } else if (dmca.type === "CLAIM_TYPE_AUDIOVISUAL") {
+        muteSection.push(
+          `volume=0:enable='between(t,${
+            dmca.matchDetails.longestMatchStartTimeSeconds
+          },${
+            parseInt(dmca.matchDetails.longestMatchDurationSeconds) +
+            parseInt(dmca.matchDetails.longestMatchStartTimeSeconds)
+          })'`
+        );
+        console.info(
+          `Trying to blackout ${
+            blackoutPath ? blackoutPath : trimmedPath
+          }. Claim: ${JSON.stringify(dmca.asset.metadata)}`
+        );
+        blackoutPath = await vod.blackoutVideo(
+          blackoutPath ? blackoutPath : trimmedPath,
+          req.body.vodId,
+          dmca.matchDetails.longestMatchStartTimeSeconds,
+          dmca.matchDetails.longestMatchDurationSeconds,
+          parseInt(dmca.matchDetails.longestMatchStartTimeSeconds) +
+            parseInt(dmca.matchDetails.longestMatchDurationSeconds)
+        );
+      }
+    }
+
+    if (muteSection.length > 0) {
+      console.info(
+        `Trying to mute ${blackoutPath ? blackoutPath : trimmedPath}`
+      );
+      newVodPath = await vod.mute(
+        blackoutPath ? blackoutPath : trimmedPath,
+        muteSection,
+        req.body.vodId
+      );
+      if (!newVodPath) return console.error("failed to mute video");
+      if (blackoutPath) fs.unlinkSync(blackoutPath);
+    }
+
+    if (!newVodPath && !blackoutPath)
+      return console.error(
+        "nothing to mute or blackout. don't try to upload.."
+      );
+
+    await vod.trimUpload(
+      newVodPath ? newVodPath : blackoutPath,
+      `${config.channel} ${vod_data.date} Vod Part ${req.body.part}`,
+      {
+        vodId: req.body.vodId,
+        youtube: vod_data.youtube,
+        part: req.body.part,
+      },
+      app
+    );
+  };
+};
+
 const fileExists = async (file) => {
   return fs.promises
     .access(file, fs.constants.F_OK)
