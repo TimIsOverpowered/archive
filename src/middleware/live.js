6c03ef5b77a6c65f0686f20e8ccdc43ce60443fc
TimIsOverpowered <admin@angelthump.com>
2021-06-02 09:10:49 +0200

    add multi track option to avoid more yt uploads

--- src/middleware/live.js
+++ src/middleware/live.js
@@ -1,4 +1,4 @@
-const vod = require('./vod');
+const vod = require("./vod");
 const config = require("../../config/config.json");
 
 module.exports = function (app) {
@@ -10,33 +10,45 @@ module.exports = function (app) {
       return res.status(400).json({ error: true, message: "No streamId" });
 
     let vods;
-    await app.service('vods')
-    .find({
-      query: {
-        stream_id: req.body.streamId
-      }
-    })
-    .then(data => {
-      vods = data.data;
-    })
-    .catch(e => {
-      console.error(e);
-    })
+    await app
+      .service("vods")
+      .find({
+        query: {
+          stream_id: req.body.streamId,
+        },
+      })
+      .then((data) => {
+        vods = data.data;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
 
-    if(vods.length == 0)
-      return res.status(404).json({error: true, message: "No Vod found"});
+    if (vods.length == 0)
+      return res.status(404).json({ error: true, message: "No Vod found" });
 
-    res.status(200).json({error:false, message: "Starting upload to youtube"});
+    res
+      .status(200)
+      .json({ error: false, message: "Starting upload to youtube" });
     const vod_data = vods[0];
-    
-    await app.service('vods')
-    .patch(vod_data.id, {
-      drive_id: req.body.driveId
-    })
-    .catch(e => {
-      console.error(e);
-    })
 
-    vod.trimUpload(`${config.livePath}/${config.channel.toLowerCase()}/${req.body.streamId}/${req.body.streamId}.mkv`, `${config.channel} ${vod_data.date} Live Vod`, false, app)
+    await app
+      .service("vods")
+      .patch(vod_data.id, {
+        drive_id: req.body.driveId,
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+    if (config.multiTrack)
+      vod.trimUpload(
+        `${config.livePath}/${config.channel.toLowerCase()}/${
+          req.body.streamId
+        }/${req.body.streamId}.mkv`,
+        `${config.channel} ${vod_data.date} Live Vod`,
+        false,
+        app
+      );
   };
 };
