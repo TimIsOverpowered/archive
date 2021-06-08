d57a6823ba7c888d22bc57ed5a28a0c8d7d8eafd
TimIsOverpowered <admin@angelthump.com>
2021-06-08 12:00:17 +0200

    add duration

--- src/middleware/live.js
+++ src/middleware/live.js
@@ -1,5 +1,6 @@
 const config = require("../../config/config.json");
 const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
 
 module.exports = function (app) {
   return async function (req, res, next) {
@@ -29,10 +30,17 @@ module.exports = function (app) {
 
     const vod_data = vods[0];
 
+    const duration = await getDuration(
+      `${config.livePath}/${config.channel.toLowerCase()}/${
+        req.body.streamId
+      }/${req.body.streamId}.mp4`
+    );
+
     await app
       .service("vods")
       .patch(vod_data.id, {
         drive_id: req.body.driveId,
+        duration: duration,
       })
       .catch((e) => {
         console.error(e);
@@ -40,13 +48,35 @@ module.exports = function (app) {
 
     res
       .status(200)
-      .json({ error: false, message: `Added Drive Id: ${req.body.drive_id} to vod ${vod_data.id}` });
+      .json({
+        error: false,
+        message: `Added Drive Id: ${req.body.drive_id} to vod ${vod_data.id}`,
+      });
 
-    await fs.promises.rmdir(
-      `${config.livePath}/${config.channel.toLowerCase()}/${req.body.streamId}`,
-      {
-        recursive: true,
-      }
-    ).catch((e) => console.error(e));
+    await fs.promises
+      .rmdir(
+        `${config.livePath}/${config.channel.toLowerCase()}/${
+          req.body.streamId
+        }`,
+        {
+          recursive: true,
+        }
+      )
+      .catch((e) => console.error(e));
   };
+};
+
+const getDuration = async (video) => {
+  let duration;
+  await new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(video, (err, metadata) => {
+      if (err) {
+        console.error(err);
+        return reject();
+      }
+      duration = metadata.format.duration;
+      resolve();
+    });
+  });
+  return duration;
 };
