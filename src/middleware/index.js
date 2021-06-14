c3446505fe8b7536601f8f34a310fadd96161838
TimIsOverpowered <admin@angelthump.com>
2021-06-14 18:35:22 +0200

    rework dmca for live vods && remove unused admin trim

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -10,8 +10,6 @@ module.exports = function (app) {
   app.post("/admin/logs/manual", admin.verify(app), admin.manualLogs(app));
   app.post("/admin/dmca", admin.verify(app), admin.dmca(app));
   app.delete("/admin/delete", admin.verify(app), admin.delete(app));
-  app.post("/admin/trim", admin.verify(app), admin.trim(app));
-  app.post("/admin/trim/dmca", admin.verify(app), admin.trimDmca(app));
   app.post("/admin/part/dmca", admin.verify(app), admin.partDmca(app));
   app.post("/admin/chapters", admin.verify(app), admin.saveChapters(app));
   app.post("/v2/admin/download", admin.verify(app), admin.downloadv2(app));
