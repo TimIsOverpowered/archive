f6e45a63e557ba2593f9b1a810f166ff4eae80d9
TimIsOverpowered <admin@angelthump.com>
2022-10-23 10:44:47 +0200

    update

--- src/middleware/live.js
+++ src/middleware/live.js
@@ -1,21 +1,20 @@
 const vod = require("./vod");
+const drive = require("./drive");
 const config = require("../../config/config.json");
-const fs = require("fs");
 
 module.exports = function (app) {
   return async function (req, res, next) {
-    if (!req.body.driveId) return res.status(400).json({ error: true, msg: "No driveId" });
+    const { streamId, path, driveId } = req.body;
+    if (!streamId) return res.status(400).json({ error: true, msg: "No streamId" });
 
-    if (!req.body.streamId) return res.status(400).json({ error: true, msg: "No streamId" });
-
-    if (!req.body.path) return res.status(400).json({ error: true, msg: "No Path" });
+    if (!path) return res.status(400).json({ error: true, msg: "No Path" });
 
     let vods;
     await app
       .service("vods")
       .find({
         query: {
-          stream_id: req.body.streamId,
+          stream_id: streamId,
         },
       })
       .then((data) => {
@@ -29,24 +28,23 @@ module.exports = function (app) {
 
     const vod_data = vods[0];
 
-    vod_data.drive.push({
-      id: req.body.driveId,
-      type: "live",
-    });
-
-    await app
-      .service("vods")
-      .patch(vod_data.id, {
-        drive: vod_data.drive,
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+    if (config.drive.upload && !driveId) {
+      drive.upload(vod_data.id, path, app, "live");
+    } else if (driveId) {
+      await app
+        .service("vods")
+        .patch(vod_data.id, {
+          drive: vod_data.drive,
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
 
     //Need to deliver a non 200 http code so it will delete the file
     if (config.youtube.multiTrack) {
       res.status(200).json({ error: false, msg: "Starting upload to youtube" });
-      await vod.upload(vod_data.id, app, req.body.path, "live");
+      await vod.upload(vod_data.id, app, path, "live");
     } else {
       res.status(404).json({ error: true, msg: "Not Uploading to youtube as per multitrack var" });
     }
