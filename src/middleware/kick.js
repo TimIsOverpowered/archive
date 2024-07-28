6f09b3da0e3b5ba0c0bfe2f8c6033e4f50d127ca
TimIsOverpowered <admin@angelthump.com>
2024-07-28 04:12:42 -0500

    hhmmss func

--- src/middleware/kick.js
+++ src/middleware/kick.js
@@ -549,3 +549,15 @@ const fileExists = async (file) => {
     .then(() => true)
     .catch(() => false);
 };
+
+const toHHMMSS = (secs) => {
+  var sec_num = parseInt(secs, 10);
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor(sec_num / 60) % 60;
+  var seconds = sec_num % 60;
+
+  return [hours, minutes, seconds]
+    .map((v) => (v < 10 ? "0" + v : v))
+    .filter((v, i) => v !== "00" || i > 0)
+    .join(":");
+};
