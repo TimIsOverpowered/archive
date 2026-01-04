5db6a51a883dceac7172296b4daf5a7354895885
TimIsOverpowered <admin@angelthump.com>
2026-01-04 10:14:34 -0600

    add platform to ilike games

--- src/services/vods/include.js
+++ src/services/vods/include.js
@@ -1,3 +1,5 @@
+const { Op } = require("sequelize");
+
 module.exports = () => {
   return async (context) => {
     const sequelize = context.app.get("sequelizeClient");
@@ -16,14 +18,61 @@ module.exports = () => {
 module.exports.games = () => {
   return async (context) => {
     const sequelize = context.app.get("sequelizeClient");
-    if (!context.params.query.chapters) return context;
-    if (!context.params.query.chapters.name) return context;
+    const game = containsGame(context.params.query);
+    if (!game) return context;
+    const platform = containsPlatform(context.params.query);
     context.params.sequelize = {
       ...context.params.sequelize,
-      where: sequelize.literal(
-        `"vods"."chapters" @? '$[*] ? (@.name like_regex ".*${context.params.query.chapters.name}.*" flag "i")'`
-      ),
+      where: {
+        [Op.and]: [
+          // 1. Use %%s to escape the percent for Sequelize
+          // 2. Wrap pattern in extra double quotes because JSONPath requires them for strings
+          sequelize.literal(
+            `"vods"."chapters" @? format('$[*] ? (@.name like_regex %s flag "i")', :gamePattern)::jsonpath`
+          ),
+          ...(platform ? [{ platform }] : []),
+        ],
+      },
+      replacements: {
+        // Must include the double quotes inside the replacement string for the JSONPath syntax
+        gamePattern: `".*${game}.*"`,
+      },
     };
     return context;
   };
+};
+
+const containsGame = (query) => {
+  if (query.$and) {
+    let game = null;
+    for (let i = 0; i <= query.$and.length; i++) {
+      let key = query.$and[i];
+      if (key?.chapters?.name) {
+        game = key.chapters.name;
+        query.$and.splice(i, 1);
+        break;
+      }
+    }
+    return game;
+  }
+  if (!query.chapters) return null;
+  if (!query.chapters.name) return null;
+  return query.chapters.name;
+};
+
+const containsPlatform = (query) => {
+  if (query.$and) {
+    let platform = null;
+    for (let i = 0; i <= query.$and.length; i++) {
+      let key = query.$and[i];
+      if (key?.platform) {
+        platform = key.platform;
+        query.$and.splice(i, 1);
+        break;
+      }
+    }
+    return platform;
+  }
+  if (!query.platform) return null;
+  return query.platform;
 };
