5cc186917177ac2b61d39666faaad63b584ed87c
TimIsOverpowered <admin@angelthump.com>
2026-05-03 15:36:27 -0500

    fix(start): run api and workers concurrently from dist, add .js extensions to dayjs plugin imports

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
-import duration from 'dayjs/plugin/duration';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import timezone from 'dayjs/plugin/timezone';
-import utcPlugin from 'dayjs/plugin/utc';
+import duration from 'dayjs/plugin/duration.js';
+import relativeTime from 'dayjs/plugin/relativeTime.js';
+import timezone from 'dayjs/plugin/timezone.js';
+import utcPlugin from 'dayjs/plugin/utc.js';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
